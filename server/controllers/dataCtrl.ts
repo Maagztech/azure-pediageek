106c7480fae0e532c5aee1ef5d3dc608cc1b8ab0
Asutosh Pradhan <75766416+Maagztech@users.noreply.github.com>
2022-07-22 19:41:52 +0530

    search

--- server/controllers/dataCtrl.ts
+++ server/controllers/dataCtrl.ts
@@ -42,10 +42,12 @@ const dataCtrl = {
     if (!req.user)
       return res.status(400).json({ msg: "Invalid Authentication." });
     try {
-      const { country } = req.body;
-      const countri = new Countries({ name: country });
-      const saved = countri.save();
-      res.json(saved);
+      console.log(req.query.country);
+      let countries = await Countries.find({
+        //updated only to convert to lower case.
+        name: { $regex: `${req.query.country}`.toLocaleLowerCase() },
+      }).limit(10);
+      res.json(countries);
     } catch (err: any) {
       return res.status(500).json({ msg: err.message });
     }
@@ -66,10 +68,12 @@ const dataCtrl = {
     if (!req.user)
       return res.status(400).json({ msg: "Invalid Authentication." });
     try {
-      const { city } = req.body;
-      const countri = new Cities({ name: city });
-      const saved = countri.save();
-      res.json(saved);
+      console.log(req.query.city);
+      let cities = await Cities.find({
+        //updated only to convert to lower case.
+        name: { $regex: `${req.query.city}`.toLocaleLowerCase() },
+      }).limit(10);
+      res.json(cities);
     } catch (err: any) {
       return res.status(500).json({ msg: err.message });
     }
@@ -90,10 +94,13 @@ const dataCtrl = {
     if (!req.user)
       return res.status(400).json({ msg: "Invalid Authentication." });
     try {
-      const { state } = req.body;
-      const countri = new States({ name: state });
-      const saved = countri.save();
-      res.json(saved);
+      console.log(req.query.state);
+      let states = await States.find({
+        //updated only to convert to lower case.
+        name: { $regex: `${req.query.state}`.toLocaleLowerCase() },
+      }).limit(10);
+
+      res.json(states);
     } catch (err: any) {
       return res.status(500).json({ msg: err.message });
     }
@@ -114,10 +121,12 @@ const dataCtrl = {
     if (!req.user)
       return res.status(400).json({ msg: "Invalid Authentication." });
     try {
-      const { work } = req.body;
-      const countri = new Works({ name: work });
-      const saved = countri.save();
-      res.json(saved);
+      console.log(req.query.work);
+      let works = await Works.find({
+        //updated only to convert to lower case.
+        name: { $regex: `${req.query.work}`.toLocaleLowerCase() },
+      }).limit(10);
+      res.json(works);
     } catch (err: any) {
       return res.status(500).json({ msg: err.message });
     }
