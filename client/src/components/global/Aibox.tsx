a567e460b760031dcc9281f6d82e793690bf0f3c
Asutosh Pradhan <75766416+Maagztech@users.noreply.github.com>
2022-07-24 16:41:32 +0530

    preferance ca't be zero

--- client/src/components/global/Aibox.tsx
+++ client/src/components/global/Aibox.tsx
@@ -83,7 +83,7 @@ const Aibox = () => {
                     <div className="modal-content">
                         <div className="modal-header">
                             <h5 className="modal-title" id="aimodalLabel">Set Preferance {catlist.length}/8 selected</h5>
-                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            
 
                         </div>
                         <div className="modal-body">
