3ebd6f53f6fe24ddc0ce92437ea80f8ba22f781e
Asutosh Pradhan <75766416+Maagztech@users.noreply.github.com>
2022-07-26 19:43:58 +0530

    perfect

--- client/src/components/ads/Adsabovepost.jsx
+++ client/src/components/ads/Adsabovepost.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import { useSelector } from "react-redux";
 
-const Abovepost = ({ imageurl }) => {
+const Abovepost = ({ imageurl, imagealt }) => {
   const { darkMode } = useSelector((state) => state);
   const { isdarkMode } = darkMode;
   useEffect(() => {
@@ -10,7 +10,8 @@ const Abovepost = ({ imageurl }) => {
 
   return (
     <div
-      className="container p-0"
+      className="container position-relative p-0"
+      title={imagealt}
       style={{
         backgroundImage: `url(${imageurl})`,
         borderRadius: 10,
@@ -21,13 +22,24 @@ const Abovepost = ({ imageurl }) => {
     >
       <ins
         className="adsbygoogle"
+        id="adsense"
         style={{ display: "block", textAlign: "center" }}
         data-ad-layout="in-article"
         data-ad-format="fluid"
         data-ad-client="ca-pub-3982561798373930"
         data-ad-slot="2009084964"
         data-full-width-responsive="false"
-      ></ins>
+      >
+        <a href={imageurl} target="_blank">
+          <span
+            class="badge rounded-pill bg-primary position-absolute"
+            style={{ bottom: "3%", right: "2%", zIndex: 8, cursor: "pointer" }}
+            id="clse"
+          >
+            View image <i className="fas fa-link"></i>
+          </span>
+        </a>
+      </ins>
     </div>
   );
 };
