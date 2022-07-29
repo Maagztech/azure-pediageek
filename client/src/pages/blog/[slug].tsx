2543811575c9a8a6c49cf5f396706f8e7737cf72
Asutosh Pradhan <75766416+Maagztech@users.noreply.github.com>
2022-07-29 09:00:13 +0530

    scroll to zero

--- client/src/pages/blog/[slug].tsx
+++ client/src/pages/blog/[slug].tsx
@@ -1,68 +1,85 @@
-import React, { useState, useEffect } from 'react'
-import { useParams } from 'react-router-dom'
-import { useSelector } from 'react-redux'
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
 
-import { IParams, IBlog, RootStore } from '../../utils/TypeScript'
-import { getAPI } from '../../utils/FetchData'
+import { IParams, IBlog, RootStore } from "../../utils/TypeScript";
+import { getAPI } from "../../utils/FetchData";
 
-import Loading from '../../components/global/Loading'
-import { showErrMsg } from '../../components/alert/Alert'
-import DisplayBlog from '../../components/blog/DisplayBlog'
-import Helmetglobal from '../../components/global/Helmetglobal'
+import Loading from "../../components/global/Loading";
+import { showErrMsg } from "../../components/alert/Alert";
+import DisplayBlog from "../../components/blog/DisplayBlog";
+import Helmetglobal from "../../components/global/Helmetglobal";
 
 const DetailBlog = () => {
-  const id = useParams<IParams>().slug
-  const { socket } = useSelector((state: RootStore) => state)
-  const [blog, setBlog] = useState<IBlog>()
-  const [loading, setLoading] = useState(false)
-  const [error, setError] = useState('')
+  const id = useParams<IParams>().slug;
+  const { socket } = useSelector((state: RootStore) => state);
+  const [blog, setBlog] = useState<IBlog>();
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
 
   useEffect(() => {
-
     if (!id) return;
 
-    setLoading(true)
+    setLoading(true);
 
     getAPI(`blog/${id}`)
-      .then(res => {
-        setBlog(res.data)
-        setLoading(false)
+      .then((res) => {
+        setBlog(res.data);
+        setLoading(false);
       })
-      .catch(err => {
-        setError(err.response.data.msg)
-        setLoading(false)
-      })
+      .catch((err) => {
+        setError(err.response.data.msg);
+        setLoading(false);
+      });
 
-    return () => setBlog(undefined)
-  }, [id])
+    return () => setBlog(undefined);
+  }, [id]);
 
   // Join Room
   useEffect(() => {
     if (!id || !socket) return;
-    socket.emit('joinRoom', id)
+    socket.emit("joinRoom", id);
 
     return () => {
-      socket.emit('outRoom', id)
-    }
-  }, [socket, id])
+      socket.emit("outRoom", id);
+    };
+  }, [socket, id]);
 
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
 
-  if (loading) return (
-    <><Helmetglobal title="Loading..." keyword="Loading" /><Loading /></>
-  );
+  if (loading)
+    return (
+      <>
+        <Helmetglobal title="Loading..." keyword="Loading" />
+        <Loading />
+      </>
+    );
   return (
     <div className="my-4">
-
-      {blog && (typeof blog.thumbnail === 'string') && (typeof blog.category === "string") &&
-        <Helmetglobal title={blog.title} description={blog.description} keyword={blog.category} twitterimage={blog.thumbnail} ogimage={blog.thumbnail} ogdescription={blog.description} ogurl={`https://www.pediageek.com/blog/${blog._id}`} twitterdescription={blog.description} ogtitle={blog.title} twittertitle={blog.title} />
-      }
+      {blog &&
+        typeof blog.thumbnail === "string" &&
+        typeof blog.category === "string" && (
+          <Helmetglobal
+            title={blog.title}
+            description={blog.description}
+            keyword={blog.category}
+            twitterimage={blog.thumbnail}
+            ogimage={blog.thumbnail}
+            ogdescription={blog.description}
+            ogurl={`https://www.pediageek.com/blog/${blog._id}`}
+            twitterdescription={blog.description}
+            ogtitle={blog.title}
+            twittertitle={blog.title}
+          />
+        )}
 
       {error && showErrMsg(error)}
 
       {blog && <DisplayBlog blog={blog} />}
-
     </div>
-  )
-}
+  );
+};
 
-export default DetailBlog
+export default DetailBlog;
